6216cdf0caf099a6eb214662f8fd5666543ff022
İbrahim Gedik <ibrahimgediktc@gmail.com>
2021-05-30 19:58:38 +0300

    edit site.webmanifest and project card links

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -15,10 +15,10 @@ function ProjectCard() {
             <div className="project-card" key={index}>
             <div className="project-card-container">
               <div className="project-card-link">
-                <a href={project.Github} className="github-link" aria-label="Project Demo Link" target="_blank">
+                <a href={project.Github} className="github-link" aria-label="Project Demo Link" target="_blank" rel="noopener">
                 <AiOutlineLink color="#fff" size={30} />
                 </a>
-              <a href={project.Demo} className="demo-link" aria-label="Project Github Link" target="_blank">
+              <a href={project.Demo} className="demo-link" aria-label="Project Github Link" target="_blank" rel="noopener">
                 <AiFillGithub color="#fff" size={30} />
                 </a>
               </div>

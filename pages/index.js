a646c4d8e40cee18bbdd27f89ec883699b7f9b85
İbrahim Gedik <ibrahimgediktc@gmail.com>
2021-05-30 17:31:59 +0300

    edit context

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,30 @@
+import React from 'react'
 import Projects from 'components/Projects'
 import Hero from '../components/Hero'
+import {getTable} from 'lib/airtable'
+import {Context} from 'config/Context'
 
-function Home() {
+function Home({data}) {
+
   return (
-  <>
+  <Context.Provider value={{data}}>
     <Hero />
     <div className="container">
       <div className="divider"></div>
     </div>
-    <Projects />
-  </>
+    <Projects  />
+  </Context.Provider>
   )
 }
+
+export async function getStaticProps(){
+  const data = await getTable('Projects');
+  return {
+      props: {
+          data,
+      },
+  }
+}
+
 
 export default Home

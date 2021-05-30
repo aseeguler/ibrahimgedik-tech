7c9e7104a96a891b8999cfbfed98c944021c712b
İbrahim Gedik <ibrahimgediktc@gmail.com>
2021-05-30 17:33:31 +0300

    create site.config.js

---
+++ site.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title:'İbrahim Gedik',
+    description:'Software Developer',
+    lang:'en',
+    siteUrl:'https://ibrahimgedik.tech',
+    author:{
+        email:'ibrahimgediktc@gmail.com',
+        name:'İbrahim Gedik'
+    },
+    social:{
+        vsco:'',
+        instagram:'',
+        twitter:'',
+        github:'',
+        linkedin:'',
+    }
+}

6284fe301c777fd0a86887175bcdb0723ddd1b4b
İbrahim Gedik <ibrahimgediktc@gmail.com>
2021-05-30 17:33:39 +0300

    update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import SiteConfig from "site.config";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -8,16 +9,34 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang={SiteConfig.lang}>
         <Head>
-          <meta name="description" content="Software Developer" />
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One"
             rel="stylesheet"
           />
           <link rel="stylesheet" href="static/font/stylesheet.css" />
+
+          <meta name="robots" content="follow, index" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="description" content={SiteConfig.description} />
+          <link rel="canonical" href={SiteConfig.siteUrl} />
+
+          <meta property="og:url" content={SiteConfig.siteUrl} />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content={SiteConfig.title} />
+          <meta property="og:description" content={SiteConfig.description} />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content={SiteConfig.siteUrl} />
+          <meta name="twitter:title" content={SiteConfig.title} />
+          <meta name="twitter:description" content={SiteConfig.description} />
+
+          <link href="/static/site.webmanifest" rel="manifest" />
+
+          <link rel="icon" href="/favicon.ico" />
+          
         </Head>
         <body>
           <Main />

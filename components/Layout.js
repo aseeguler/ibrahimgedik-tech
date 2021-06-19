9a214f0f87a82223514097ad3773cf719060ab72
İbrahim Gedik <ibrahimgediktc@gmail.com>
2021-06-19 13:50:32 +0300

    add animation main component

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,28 @@
 import React from "react";
 import Footer from "./Footer";
 import Navigation from "./Navigation";
+import {motion} from 'framer-motion'
 
 function Layout({ children }) {
   return (
     <>
       <Navigation />
-      <main className="main">{children}</main>
+        <motion.main 
+        className="main"
+        initial={{
+          opacity:0,
+          y:100
+        }}
+        animate={{
+          opacity:1,
+          y:0
+        }}
+        transition={{
+          delay:0.2,
+          duration:0.5
+        }}
+        >{children}
+        </motion.main>
       <Footer />
     </>
   );

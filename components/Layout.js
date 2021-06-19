42c909b4b5bff78cc1fda074b2fb9a8887f011b3
İbrahim Gedik <ibrahimgediktc@gmail.com>
2021-06-19 22:59:38 +0300

    update animation

--- components/Layout.js
+++ components/Layout.js
@@ -11,14 +11,14 @@ function Layout({ children }) {
         className="main"
         initial={{
           opacity:0,
-          y:100
+          y:-100
         }}
         animate={{
           opacity:1,
           y:0
         }}
         transition={{
-          delay:0.2,
+          // delay:0.1,
           duration:0.5
         }}
         >{children}

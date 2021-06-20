0acbfbd035fe6701ee56c7d199f96401996e43b3
İbrahim Gedik <ibrahimgediktc@gmail.com>
2021-06-20 16:42:08 +0300

    update hero

--- components/Hero.js
+++ components/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
             <div className="container">
                 <h3 className="hero-title">Hi, I'm</h3>
                 <h1 className="hero-subtitle">ibrahim</h1>
-                <p className="hero-description">I'm a <a href="https://www.linkedin.com/in/ibrahimgedik/" className="note" aria-label="My Linkedin Account" target="_blank">Software Developer</a>, <a href="https://vsco.co/ibrahimgedik/gallery" className="note" aria-label="My Vsco.co Account" target="_blank">Amateur Photographer</a> and Dreamer based in Istanbul, Turkey.
+                <p className="hero-description">I'm a <a href="https://www.linkedin.com/in/ibrahimgedik/" className="note" aria-label="My Linkedin Account" target="_blank" rel="noopener">Software Developer</a> and <a href="https://vsco.co/ibrahimgedik/gallery" className="note" aria-label="My Vsco.co Account" target="_blank" rel="noopener">Amateur Photographer</a> based in Istanbul, Turkey.
                 </p>
             </div>
         </section>

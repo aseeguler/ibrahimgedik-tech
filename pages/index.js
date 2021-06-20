c99a14992407410b032a1915c9430bdb3eb949bc
İbrahim Gedik <ibrahimgediktc@gmail.com>
2021-06-20 14:30:34 +0300

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,32 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import Musics from 'components/Musics'
-import {getTable} from 'lib/airtable'
-import {Context} from 'config/Context'
+import React from "react";
+import Hero from "../components/Hero";
+import Musics from "components/Musics";
+import { getTable } from "lib/airtable";
+import { Context } from "config/Context";
 
-function Home({data}) {
+function Home({ data }) {
   return (
-  <Context.Provider value={{data}}>
-    <Hero />
-    <div className="container">
-      <div className="divider"></div>
-    </div>
-    <Musics />
-    <div className="container">
-      <div className="divider"></div>
-    </div>
-    <div>
-
-    </div>
-  </Context.Provider>
-  )
+    <Context.Provider value={{ data }}>
+      <Hero />
+      <div className="container">
+        <div className="divider"></div>
+      </div>
+      <Musics />
+      <div className="container">
+        <div className="divider"></div>
+      </div>
+    </Context.Provider>
+  );
 }
 
-export async function getStaticProps(){
-  const data = await getTable('Musics');
+export async function getStaticProps() {
+  const data = await getTable("Musics");
   return {
-      props: {
-          data,
-      },
-      revalidate: 600,
-  }
+    props: {
+      data,
+    },
+    revalidate: 600,
+  };
 }
 
-
-export default Home
+export default Home;
